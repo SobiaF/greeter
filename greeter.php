285b63b6cd4baedece8ec0f757261bcfc4e3bbf0
SobiaF <sobiafurqan@hotmail.com>
2021-03-19 16:09:42 +0000

    Create greeter.php

---
+++ greeter.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @param Create a function that accepts two parameters - a string with first name and string with surname
+
+ * @return a greeting message something like 'Welcome firstname surname!'
+ */
+
+function greeter($fname, $lname)
+{
+    echo "Welcome, $fname $lname!";
+};
+
+greeter('Selena', 'Khan');
