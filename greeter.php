2d504aed039b20d92ab5d76564a90f2654ea24f2
SobiaF <sobiafurqan@hotmail.com>
2021-04-02 20:25:05 +0100

    Update greeter.php

--- greeter.php
+++ greeter.php
@@ -1,10 +1,21 @@
 <?php
-
 /**
- * @param Create a function that accepts two parameters - a string with first name and string with surname
-
- * @return a greeting message something like 'Welcome firstname surname!'
+ * @param string $fname
+ * @param string $lname optional
+ * @return string
  */
+function greeter(string $fname, string $lname) : string
+{
+if ($lname) {
+return '<h2>Welcome ' . $fname . ' ' . $lname . ' !</h2';
+} else {
+    return '<h2>Welcome ' . $fname .' !</h2';
+}
+}
+
+$welcomeMessage = greeter('Selena', 'Khan');
+echo $welcomeMessage;
+
 
 function greeter($fname, $lname)
 {
